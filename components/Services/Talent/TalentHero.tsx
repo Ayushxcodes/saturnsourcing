a84c7aa1797861b5a2a3a14dfdaf623739c28014
Ayush Pandey <en22cs301251@medicaps.ac.in>
2025-12-09 22:33:58 +0530

    fixes

--- components/Services/Talent/TalentHero.tsx
+++ components/Services/Talent/TalentHero.tsx
@@ -164,15 +164,13 @@ export default function TalentHero() {
 								Simplify hiring, payroll compliance & benefits for talent anywhere
 							</h2>
 							<ul className="grid grid-cols-1 gap-3 text-sm md:text-base mt-2">
-								<li>Hire and pay compliantly in 120+ countries</li>
-								<li>Streamline global payments in 120+ currencies</li>
+								<li>Hire and pay compliantly in india</li>
+								<li>Streamline global payments in indian currency</li>
 								<li>Generate 100% legal-reviewed compliant agreements</li>
 								<li>Onboard new talent in as fast as 48 hours</li>
 								<li>Offer competitive salary, equity, and benefits</li>
 							</ul>
-							<Button variant="link" size="sm" className="w-fit mt-2">
-								Learn More
-							</Button>
+							
 						</div>
 						{/* Image Right */}
 						<div className="flex-1 flex justify-center items-center max-w-lg mb-8 md:mb-0">
@@ -229,8 +227,8 @@ export default function TalentHero() {
 										SaturnConsultingGroup’s easy-to-use self-serve platform puts everything you need at your fingertips, allowing you to hire, pay, and manage contractors across 180+ countries—all in one place.
 									</p>
 									<ul className="grid grid-cols-1 gap-3 text-sm md:text-base mt-4">
-										<li>Hire contractors compliantly across 180+ countries.</li>
-										<li>Pay contractors in 120+ currencies with &gt;99% accuracy.</li>
+										<li>Hire contractors compliantly across India.</li>
+										<li>Pay contractors in indian currency with &gt;99% accuracy.</li>
 										<li>Generate compliant contracts in minutes.</li>
 										<li>Manage invoices, expenses, and contract terms in one dashboard.</li>
 										<li>Add misclassification protection up to USD 500,000.</li>
@@ -304,7 +302,7 @@ export default function TalentHero() {
 								{/* Info Left */}
 								<div className="flex-1 flex flex-col gap-4 max-w-lg">
 									<h3 className="text-xl md:text-2xl font-semibold text-primary">
-										Fast payments in 120+ currencies
+										Fast payments in indian currency
 									</h3>
 									<p className="text-sm md:text-base text-muted-foreground">
 										Whether it's pay-as-you-go (PAYG), or fixed payment schedules, SaturnConsultingGroup simplifies payments across borders. Pay your contractors on-time and in the right currency, no matter the payment structure.
